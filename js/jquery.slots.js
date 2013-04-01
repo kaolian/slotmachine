02edbc6d0e58c0897fe7233712983830ff62608f
Sangjin Lim <dannyjin@stylesha.re>
2013-04-01 22:26:31 +0900

    Make 4 wheels with digit

--- js/jquery.slots.js
+++ js/jquery.slots.js
@@ -3,22 +3,15 @@
   // ready? .. set, go!
 
   var transitionend = "webkitTransitionEnd transitionend";
-  var slotsTypes = {
-    'cherry': [0, 50, 100],
-    'orange': [0, 100, 200],
-    'prune': [0, 100, 200],
-    'bell': [0, 100, 200],
-    'bar': [0, 200, 500],
-    'seven': [0, 0, 1000]
-  };
   var slots = [
-    ['bell', 'orange', 'prune', 'orange', 'seven', 'cherry', 'bar', 'bell', 'prune'],
-    ['prune', 'orange', 'seven', 'cherry', 'bar', 'bell', 'prune', 'orange', 'cherry'],
-    ['orange', 'bell', 'orange', 'prune', 'orange', 'seven', 'cherry', 'bar', 'bell']
+    [0, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    [0, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    [0, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
   ];
   var credits = 5;
   var points = 0;
-  var spin = [0, 0, 0];
+  var spin = [0, 0, 0, 0];
   var result = [];
   var rotateTimer;
   var count = 0;
@@ -40,7 +33,6 @@
     });
   };
 
-
   var endSpin = function (el, match, pointCount, creditCount) {
     var ended = 0;
     var matches = 1;
@@ -50,15 +42,17 @@
       matches++;
       if(match[0] === match[2]) {
         matches++;
+        if(match[0] === match[3]) {
+          matches++;
+        }
       }
     }
-    //console.log(match);
     creditCount.html(credits);
     el.on(transitionend, function () {
       allowPlay = true;
       ended++;
-      if(ended === 3) {
-        var winPoints = slotsTypes[match[0]][matches - 1];
+      if(ended === 4) {
+        var winPoints = matches * 100;
         points += winPoints;
         if(winPoints > 0) {
           addPoints(pointCount, winPoints);
@@ -67,9 +61,7 @@
     });
   };
 
-
   $(function () {
-
     var frame = $('#page');
     var winBox = $('#win');
     var creditBox = $('#credits');
@@ -77,16 +69,12 @@
     var wheels = $('.wheel');
     creditBox.html(credits);
 
-
-    //control turning
-
     var rotation = $('#rotation');
     var perspective = $('#perspective');
     frame.addClass('turn-360');
     setTimeout(function () {
       frame.removeClass('turn-360');
     }, 3600);
-
     rotation.on('change', function () {
       var degree = $(this).val();
       var view = perspective.val();
@@ -95,7 +83,6 @@
         WebkitTransform: 'perspective(' + view + ') rotateY(' + degree + 'deg) translate3d(0,0,0)'
       });
     });
-
     perspective.on('change', function () {
       var degree = rotation.val();
       var view = $(this).val();
@@ -103,38 +90,17 @@
         MozTransform: 'perspective(' + view + ') rotateY(' + degree + 'deg) translate3d(0,0,0)',
         WebkitTransform: 'perspective(' + view + ') rotateY(' + degree + 'deg) translate3d(0,0,0)'
       });
-
     });
 
-
-    //@end turning*/
-
-
-
-
-
-
-    // define for each wheel
     wheels.each(function () {
       var $this = $(this);
-      var randomNumber = (parseInt((Math.random() * 10), 10));
       var zero = 0;
       var index = $this.index();
       var spinPlus = 0;
 
-
-
       play.on('click', function () {
-        //randomNumber = parseInt((Math.random() * 10),10);
         if(credits > 0 && allowPlay) {
-
           var type = parseInt((Math.random() * 9), 10);
-
-          // if (type > 8) {
-          //   var diff = type - 9;
-          //   type = diff;
-          // }
-          randomNumber += type;
           var duration = parseInt((Math.random() * 10000), 10);
           if(duration < 1000) {
             duration *= 10;
@@ -143,7 +109,8 @@
             duration += 5000;
           }
           spinPlus += 3600;
-          var rotateWheel = (type + 1) * 40 + spinPlus;
+          var rotateWheel = (type + 1) * 36 + spinPlus;
+          console.log((10-type) % 10);
           if(zero < 1) {
             duration = 0;
             zero += 1;
@@ -151,7 +118,7 @@
           else {
             result.push(slots[index][type]);
             count++;
-            if(count === 3) {
+            if(count === 4) {
               endSpin(wheels, result, winBox, creditBox);
               count = 0;
               result = [];
